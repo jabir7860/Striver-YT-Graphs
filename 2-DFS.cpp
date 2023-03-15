c8a119bd59f286d662ccd1fcbce53b3b18bf0237
jabir7860 <79660697+jabir7860@users.noreply.github.com>
2023-03-15 16:16:26 +0530

    Update 2-DFS.cpp

--- 2-DFS.cpp
+++ 2-DFS.cpp
@@ -1,1 +1,20 @@
-https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=dfs_of_graph
+// https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=dfs_of_graph
+
+void dfs(int node,vector<int>adj[],int vis[],vector<int>&ans)
+    {
+        vis[node]=1; 
+        ans.push_back(node); 
+        for(auto i:adj[node]) 
+        {
+            if(!vis[i]) 
+            {
+                dfs(i,adj,vis,ans); 
+            }
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>ans; 
+        int vis[V+1]={0}; 
+        dfs(0,adj,vis,ans); 
+        return ans; 
+    }
