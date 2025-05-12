f71e41ef11e9ac33c7e1928ce48d0ea5bf8aecc4
Mohammad Jabir ☑️ <79660697+jabir7860@users.noreply.github.com>
2025-05-12 20:05:02 +0530

    Update 3-No of Provinces.cpp

--- 3-No of Provinces.cpp
+++ 3-No of Provinces.cpp
@@ -1,41 +1,54 @@
 https://practice.geeksforgeeks.org/problems/number-of-provinces/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=number_of_provinces
+https://www.geeksforgeeks.org/problems/connected-components-in-an-undirected-graph/1 
 
-private:
-    void dfs(int node,vector<int>adjList[],int vis[])
+class Solution {
+  public:
+    void dfs(int i,vector<int>adj[],vector<int>&vis,vector<vector<int>>&ans) 
     {
-        vis[node]=1; 
-        for(auto i:adjList[node])
+        vis[i]=1; 
+        queue<int>q; 
+        q.push(i); 
+        vector<int>temp; 
+        while(!q.empty()) 
         {
-            if(!vis[i])
+            int node=q.front(); 
+            q.pop(); 
+            temp.push_back(node); 
+            for(auto j:adj[node]) 
             {
-                dfs(i,adjList,vis); 
-            }
-        }
-    }
-    public:
-    int numProvinces(vector<vector<int>> adj, int V) {
-        // convert given 2D adj to 1D adj 
-        vector<int>adjList(V); 
-        for(int i=0;i<=V;i++)
-        {
-            for(int j=0;j<=V;j++)
-            {
-                if(i!=j && adj[i][j]==1)
+                if(!vis[j]) 
                 {
-                    adjList[i].push_back(j); 
-                    adjList[j].push_back(i); 
+                    vis[j]=1;
+                    q.push(j); 
                 }
             }
         }
-        int vis[V+1]={0}; 
-        int count=0; 
-        for(int i=0;i<=V;i++)
+        sort(temp.begin(),temp.end()); 
+        ans.push_back(temp); 
+    }
+    vector<vector<int>> getComponents(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>>ans; 
+        vector<int>adj[V]; 
+        for(int i=0;i<edges.size();i++) 
         {
-            if(!vis[i])
+            adj[edges[i][0]].push_back(edges[i][1]); 
+            adj[edges[i][1]].push_back(edges[i][0]); 
+
+        }
+        vector<int>vis(V,0); 
+        int count=0; 
+        for(int i=0;i<V;i++) 
+        {
+            if(!vis[i]) 
             {
+                dfs(i,adj,vis,ans);
                 count++; 
-                dfs(i,adjList,vis); 
             }
         }
-        return count; 
+        cout<<"NO of Provisions : "<<count<<endl ;
+        return ans; 
     }
+};
+
+TC : O(V+E)
